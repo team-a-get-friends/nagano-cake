a242780ae0e87cd526ea4d1b81b819151066dff7
dooryanagi <166465828+dooryanagi@users.noreply.github.com>
2024-06-20 11:45:22 +0900

    Update order_detail.rb
    ：を追加しました

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,12 +1,12 @@
 class OrderDetail < ApplicationRecord
   #注文ID
-  t.integer order_id, null: false
+  t.integer :order_id, null: false
   #商品ID
-  t.integer item_id, null: false
+  t.integer :item_id, null: false
   #購入時価格(税込)
-  t.integer price, null: false
+  t.integer :price, null: false
   #数量
-  t.integer amount, null: false
+  t.integer :amount, null: false
   #製作ステータス
-  t.integer making_status, default: 0, null: false
+  t.integer :making_status, default: 0, null: false
 end
