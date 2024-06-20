b5e6bc710ba189d61d72c862c4ccc72155810c2c
dooryanagi <166465828+dooryanagi@users.noreply.github.com>
2024-06-20 02:27:12 +0000

    [Finish]add_address_column

--- db/migrate/20240620012459_create_items.rb
+++ db/migrate/20240620012459_create_items.rb
@@ -2,6 +2,12 @@ class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
 
+      t.integer :genre_id, null: false
+      t.string :name, null: false
+      t.text :item_detail, null: false
+      t.boolean :is_active, null: false, default: true
+      t.integer :price, null: false
+
       t.timestamps
     end
   end
