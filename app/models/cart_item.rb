f9e98aeb060d159138f4f66f0a697bd7ef6344f7
dooryanagi <166465828+dooryanagi@users.noreply.github.com>
2024-06-20 11:41:24 +0900

    Update cart_item.rb
    スペルミスを修正しました

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,8 +1,8 @@
 class CartItem < ApplicationRecord
   #商品ID
-  t.inteder item_id, null: false
+  t.integer item_id, null: false
   #会員ID
-  t.inteder customer_id, null: false
+  t.integer customer_id, null: false
   #数量
   t.integer amount, null: false
 end
