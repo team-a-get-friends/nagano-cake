d8f16d7b0c2aa5a85eebed0ebaed152dbd9aff0b
dooryanagi <166465828+dooryanagi@users.noreply.github.com>
2024-06-20 08:24:32 +0000

    [Finish]0620

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 管理者のログイン情報は直接DBに記述
+Admin.create!(
+	email: 'admin@friend',
+	password: 'friend'
+)
