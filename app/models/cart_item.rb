865137a8db79e05ae9e7dedd0a58fa9ba54f714b
dda0dd <168615435+dda0dd@users.noreply.github.com>
2024-06-20 02:27:00 +0000

    [Add] カラム追記

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,8 @@
 class CartItem < ApplicationRecord
+  #商品ID
+  t.inteder item_id, null: false
+  #会員ID
+  t.inteder customer_id, null: false
+  #数量
+  t.integer amount, null: false
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,4 @@
 class Genre < ApplicationRecord
+  #ジャンル名
+  t.string name, null: false
 end
