c4cc8b5f7ff839010f0ca383795dd740c81b2a61
151339900 <165742289+151339900@users.noreply.github.com>
2024-06-21 06:52:58 +0000

    debug items_new

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,21 +2,20 @@ class Item < ApplicationRecord
 
   has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
+  has_one_attached :item_image
   belongs_to :genre
 
   validates :genre_id, presence:true
   validates :name, presence:true
   validates :item_detail, presence:true
-  validates is_active, inclusion: [ture, false]
+  validates :is_active, inclusion: [true, false]
   validates :price, presence:true
 
-　has_one_attached :item_image
+
 
   def get_item_image(width,height)
   	item_image.variant(resize_to_limit: [width,height]).processed
   	(profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
-
-　#検索機能を追加するときはに定義
-
+  #検索機能を追加するときはに定義
 end
