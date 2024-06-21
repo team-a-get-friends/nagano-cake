aebf89c1da1ca6844b2f4a227a4f489035ad9111
dda0dd <168615435+dda0dd@users.noreply.github.com>
2024-06-21 08:43:37 +0000

    [Add]管理者の顧客詳細情報追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,8 +4,10 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  #顧客の複数の注文(customer : orders) 1:Nの関係
   has_many :orders, dependent: :destroy
   has_many :cart_items, dependent: :destroy
   has_many :addresses, dependent: :destroy
 
+  
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ApplicationRecord
-  
+
+  #顧客の注文履歴一覧(orderはcusotmerに属している)
   belongs_to :customer
   has_many :order_details, dependent: :destroy
-  
+
 end
