d957efdbbbddd2cd98d9b2b46e11b8d306a1979a
dooryanagi <166465828+dooryanagi@users.noreply.github.com>
2024-06-21 08:20:10 +0000

    [NotFinish]setting_order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,10 @@
 class Order < ApplicationRecord
-  
+
   belongs_to :customer
   has_many :order_details, dependent: :destroy
-  
+
+  # enumに関わる記述
+  enum payment_method: { credit_card: 0, transfer: 1 }
+  enum status: { waiting_for_payment: 0, confirming_payment: 1, in_making: 2, preparing: 3, completed: 4 }
+
 end
