877cb3b7b47190d264225f0c71761518039b2b1b
dda0dd <168615435+dda0dd@users.noreply.github.com>
2024-06-22 02:55:34 +0000

    [Add]商品詳細情報

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,21 +1,24 @@
 class Admin::ItemsController < ApplicationController
-  
+
   def index
   end
-  
+
   def new
   end
-  
+
   def create
   end
-  
+
+  #商品詳細情報
   def show
+    # Itemsのsを削除(Name Error発生)
+    @item = Item.find(params[:id])
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,8 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
   belongs_to :genre
+  
+  #Active Storage属性追加(画像)
+  has_one_attached :image
 
 end
