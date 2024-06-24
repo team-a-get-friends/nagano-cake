a3bfbdbc297a932fe2d15a577e70c1a7398e6708
dooryanagi <166465828+dooryanagi@users.noreply.github.com>
2024-06-24 05:32:22 +0000

    [Finish]setting_address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,10 +1,14 @@
 class Address < ApplicationRecord
-  
+
   belongs_to :customer
-  
+
+  validates :post_code, presence:true
+  validates :address, presence:true
+  validates :name, presence:true
+
   # 登録している情報から住所を一連で表示させるためのメソッド
   def address_display
   '〒' + postal_code + ' ' + address + ' ' + name
   end
-  
+
 end
